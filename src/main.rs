ce33a923e9b39d8b06c54afcc1dce4d88d761c75
zaimoni <none@none>
2020-06-18 21:09:02 -0500

    switch to actor class aware event handling

--- src/main.rs
+++ src/main.rs
@@ -7,59 +7,6 @@ use std::rc::Rc;
 
 // this is going to lift to another file eventually
 // We likely should be passing a Player or PlayerController object
-fn handle_events(r: &mut Root, pl_scrpos: &mut [i32;2]) -> bool {
-    use tcod::input::{Key, KeyCode /*,EventFlags,check_for_event*/};
-
-    const legal_screenpos_x: i32 = VIEW;
-    const legal_screenpos_y: i32 = VIEW;
-
-//  let ev = check_for_event(EventFlags::Keypress);
-    let key = r.wait_for_keypress(true);    // could r.check_for_keypress instead but then would have to pause/multi-process explicitly
-
-    match key {
-        Key { code: KeyCode::Enter, alt: true, .. } => {
-            // Alt+Enter: toggle fullscreen
-            let fullscreen = r.is_fullscreen();
-            r.set_fullscreen(!fullscreen);
-        },
-        Key { code: KeyCode::Escape, .. } => return true,
-        // movement keys
-        Key { code: KeyCode::Up, .. } => (*pl_scrpos)[1] -= 1,
-        Key { code: KeyCode::NumPad8, .. } => (*pl_scrpos)[1] -= 1,
-        Key { code: KeyCode::Down, .. } => (*pl_scrpos)[1] += 1,
-        Key { code: KeyCode::NumPad2, .. } => (*pl_scrpos)[1] += 1,
-        Key { code: KeyCode::Left, .. } => (*pl_scrpos)[0] -= 1,
-        Key { code: KeyCode::NumPad4, .. } => (*pl_scrpos)[0] -= 1,
-        Key { code: KeyCode::Right, .. } => (*pl_scrpos)[0] += 1,
-        Key { code: KeyCode::NumPad6, .. } => (*pl_scrpos)[0] += 1,
-        Key { code: KeyCode::NumPad7, .. } => {
-            (*pl_scrpos)[0] -= 1;
-            (*pl_scrpos)[1] -= 1;
-        },
-        Key { code: KeyCode::NumPad9, .. } => {
-            (*pl_scrpos)[0] += 1;
-            (*pl_scrpos)[1] -= 1;
-        },
-        Key { code: KeyCode::NumPad1, .. } => {
-            (*pl_scrpos)[0] -= 1;
-            (*pl_scrpos)[1] += 1;
-        },
-        Key { code: KeyCode::NumPad3, .. } => {
-            (*pl_scrpos)[0] += 1;
-            (*pl_scrpos)[1] += 1;
-        },
-
-        _ => {}
-    }
-    if legal_screenpos_x <= pl_scrpos[0] { (*pl_scrpos)[0] = legal_screenpos_x-1; }
-    else if 0 > pl_scrpos[0] { (*pl_scrpos)[0] = 0; }
-
-    if legal_screenpos_y <= pl_scrpos[1] { (*pl_scrpos)[1] = legal_screenpos_y-1; }
-    else if 0 > pl_scrpos[1] { (*pl_scrpos)[1] = 0; }
-
-    return false;
-}
-
 fn handle_events_pc(r: &mut Root, w:&World, pc:&mut Actor) -> bool {
     use tcod::input::{Key, KeyCode /*,EventFlags,check_for_event*/};
     debug_assert!(pc.is_pc);
@@ -146,11 +93,11 @@ fn main() {
 
     while !dm.root.window_closed() {
         dm.clear();
-        dm.draw(&player_screenpos, player.borrow().fg());
+        dm.draw(&player.borrow().loc().pos, player.borrow().fg());
         dm.render();
 
         // Handling user input
-        if handle_events(&mut dm.root, &mut player_screenpos) { return; }
+        if handle_events_pc(&mut dm.root, &world, &mut player.borrow_mut()) { return; }
 
         // Updating the gamestate
         // Rendering the results
